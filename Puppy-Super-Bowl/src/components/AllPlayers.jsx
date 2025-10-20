9bd12fa20732cfd7d7a3b98b79086089bdcfd90d
kiannadalton <kiannadalton@aol.com>
2025-10-20 14:42:53 -0400

    Added baseline structure to Puppy Super Bowl Workshop

---
+++ Puppy-Super-Bowl/src/components/AllPlayers.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export default function AllPlayers() {
+    //   const [players, setPlayers] = useState();
+
+  return <div>
+    <h2>All Players</h2>
+  </div>;
+}
---
+++ Puppy-Super-Bowl/src/components/SinglePlayer.jsx
@@ -0,0 +1,6 @@
+export default function SinglePlayer(){
+
+    return (<div>
+        <h2>Single Player Card</h2>
+    </div>)
+}
