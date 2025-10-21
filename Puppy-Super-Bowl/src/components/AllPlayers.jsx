e5561183cc0caa41878ae76fb756b0ae1c03d46a
kiannadalton <kiannadalton@aol.com>
2025-10-21 13:55:43 -0400

    Added file structure for components with filler information. All components show when running npm run dev

--- Puppy-Super-Bowl/src/components/AllPlayers.jsx
+++ Puppy-Super-Bowl/src/components/AllPlayers.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import SearchBar from "./SearchBar";
 
 export default function AllPlayers() {
-    //   const [players, setPlayers] = useState();
+  //   const [players, setPlayers] = useState();
 
-  return <div>
-    <h2>All Players</h2>
-  </div>;
+  return (
+    <div>
+      <h2>All Players</h2>
+      <SearchBar />
+    </div>
+  );
 }
---
+++ Puppy-Super-Bowl/src/components/SearchBar.jsx
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+  return (
+    <div>
+      <h2>SearchBar</h2>
+    </div>
+  );
+}
